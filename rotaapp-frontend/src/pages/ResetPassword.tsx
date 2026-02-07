2afca34c6b0596d82362bc1dff361f5291dc2b26
ucarere <ucarere@gmail.com>
2026-02-07 22:34:18 +0300

    Fix confirm password ternary

--- rotaapp-frontend/src/pages/ResetPassword.tsx
+++ rotaapp-frontend/src/pages/ResetPassword.tsx
@@ -138,7 +138,7 @@ const ResetPassword: React.FC = () => {
               <div className="relative">
                 <Lock className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400" />
                 <input
-                  type={showPassword  'text' : 'password'}
+                  type={showPassword ? 'text' : 'password'}
                   value={formData.confirmPassword}
                   onChange={(e) => setFormData({...formData, confirmPassword: e.target.value})}
                   className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
